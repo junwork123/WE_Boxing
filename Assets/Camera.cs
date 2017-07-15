f034d75429c28b32670580493c0341c704210c26
MaliciousJ <sposent7@naver.com>
2017-07-15 12:46:29 +0900

    project upload

---
+++ Assets/Camera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class angle : MonoBehaviour
+{
+    public float sensitivity = 100.0f;
+    float rotationX;
+    float rotationY;
+    float mouseMoveValueX;
+    float mouseMoveValueY;
+
+    // Use this for initialization
+    void Start()
+    {
+        Transform target = GameObject.Find("enermy").transform;
+        transform.LookAt(target);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        /*
+		if (Input.GetMouseButton(0)) {
+			mouseMoveValueX = Input.GetAxis ("Mouse X");
+			mouseMoveValueY = Input.GetAxis ("Mouse Y");
+
+			rotationY += mouseMoveValueX * sensitivity * Time.fixedDeltaTime;
+			rotationX += mouseMoveValueY * sensitivity * Time.fixedDeltaTime;
+
+			rotationY %= 360;
+			rotationX %= 360;
+
+			rotationX = Mathf.Clamp (rotationX, -140.0f, 140.0f);
+			rotationY = Mathf.Clamp (rotationY, -140.0f, 140.0f);
+			transform.eulerAngles = new Vector3 (-rotationX, rotationY, 0.0f);
+
+		}
+	*/
+    }
+}
---
+++ Assets/Scripts/BloodRepeatScript.cs
@@ -0,0 +1,54 @@
+﻿/* BloodRepeatScript.cs */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodRepeatScript : MonoBehaviour {
+    public GameObject prefab = null;
+    GameObject target;
+
+    private AudioSource hsAudio;
+    public AudioClip Right_Hook;
+
+    private AudioSource hsAudio2;
+    public AudioClip Jab;
+
+    // Use this for initialization
+    void Start () {
+
+        target = GameObject.FindGameObjectWithTag("Enermy");
+
+        this.hsAudio = this.gameObject.AddComponent<AudioSource> ();
+        this.hsAudio.clip = this.Right_Hook;
+        this.hsAudio.loop = false;
+
+        this.hsAudio2 = this.gameObject.AddComponent<AudioSource>();
+        this.hsAudio2.clip = this.Jab;
+        this.hsAudio2.loop = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+        if(Input.GetMouseButton(0))
+        {
+
+            Instantiate(prefab, target.transform);
+            this.hsAudio.Play();
+            Destroy(this.prefab);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Instantiate(prefab, target.transform);
+            this.hsAudio2.Play();
+            Destroy(this.prefab);
+        }
+        
+        
+    }
+    void Move()
+    {
+        prefab.transform.rotation = Quaternion.Slerp(prefab.transform.rotation,
+        Quaternion.LookRotation(target.transform.position - prefab.transform.position), 1);
+    }
+}
